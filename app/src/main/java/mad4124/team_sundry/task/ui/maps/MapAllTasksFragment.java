a50d565886cbc9fcc31798cb606a7cb6b5808b34
Alice <c0874203@mylambton.ca>
2023-02-08 02:04:22 -0500

    implement google map service and map view.

--- app/src/main/java/mad4124/team_sundry/task/ui/maps/MapAllTasksFragment.java
+++ app/src/main/java/mad4124/team_sundry/task/ui/maps/MapAllTasksFragment.java
@@ -1,9 +1,17 @@
 package mad4124.team_sundry.task.ui.maps;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
-public class MapAllTasksFragment extends Fragment {
+public class MapAllTasksFragment extends Fragment implements OnMapReadyCallback {
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        
+    }
 }

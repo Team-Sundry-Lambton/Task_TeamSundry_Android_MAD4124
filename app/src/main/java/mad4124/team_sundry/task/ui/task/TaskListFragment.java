e790cbd7f143f7a9dd5c1983437ada9fe2b657e5
0871063 <c0871063@mylambton.ca>
2023-02-09 17:50:55 -0500

    Bind Fragment

--- app/src/main/java/mad4124/team_sundry/task/ui/task/TaskListFragment.java
+++ app/src/main/java/mad4124/team_sundry/task/ui/task/TaskListFragment.java
@@ -1,9 +1,69 @@
 package mad4124.team_sundry.task.ui.task;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import dagger.hilt.android.AndroidEntryPoint;
+import mad4124.team_sundry.task.R;
+import mad4124.team_sundry.task.databinding.FragmentTaskListBinding;
+import mad4124.team_sundry.task.model.Task;
+import mad4124.team_sundry.task.ui.taskDetail.TaskDetailFragment;
 
 @AndroidEntryPoint
 public class TaskListFragment extends Fragment {
+    public static final String TASK_NAME = "task_name";
+    private FragmentTaskListBinding binding;
+
+    private List<Task> taskList = new ArrayList<>();
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentTaskListBinding.inflate(inflater, container, false);
+        binding.recyclerView.setHasFixedSize(true);
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2, RecyclerView.VERTICAL,false));
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(TaskListFragment.this)
+                        .navigate(R.id.action_taskListFragment_to_taskDetailFragment);
+            }
+        });
+    }
+
+    void setDataSource(List<Task> departmentsWithEmployees) {
+        taskList.clear();
+        taskList.addAll(departmentsWithEmployees);
+        Log.d("set source", "setDataSource: ");
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+        Log.d("TAG", "onDestroyView: ");
+    }
 }

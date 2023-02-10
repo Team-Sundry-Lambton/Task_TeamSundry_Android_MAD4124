4f0270d3dbe0326be67bed44c8c1c2871c16adb9
Alice <c0874203@mylambton.ca>
2023-02-09 21:30:55 -0500

    fix PR issue.

--- app/src/main/java/mad4124/team_sundry/task/utils/MyNotificationPublisher.java
+++ app/src/main/java/mad4124/team_sundry/task/utils/MyNotificationPublisher.java
@@ -14,13 +14,14 @@ import android.widget.Toast;
 public class MyNotificationPublisher extends BroadcastReceiver {
     public static String NOTIFICATION_ID = "notification-id";
     public static String NOTIFICATION = "notification";
+    private static String NOTIFICATION_CHANNEL_NAME = "Notification_Task_App";
 
     public void onReceive(Context context, Intent intent) {
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         Notification notification = intent.getParcelableExtra(NOTIFICATION);
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             int importance = NotificationManager.IMPORTANCE_HIGH;
-            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "NOTIFICATION_CHANNEL_NAME", importance);
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_NAME, importance);
             assert notificationManager != null;
             notificationManager.createNotificationChannel(notificationChannel);
         }

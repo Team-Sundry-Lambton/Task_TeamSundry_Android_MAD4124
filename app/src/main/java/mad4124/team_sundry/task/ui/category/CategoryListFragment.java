363fdd99d296d1ec2c2ad58fdc5e433bf7f23729
Sonia364 <c0872364@mylambton.ca>
2023-02-10 08:41:39 -0500

    Implemented Toolbar on CategoryList layout

--- app/src/main/java/mad4124/team_sundry/task/ui/category/CategoryListFragment.java
+++ app/src/main/java/mad4124/team_sundry/task/ui/category/CategoryListFragment.java
@@ -1,9 +1,28 @@
 package mad4124.team_sundry.task.ui.category;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import dagger.hilt.android.AndroidEntryPoint;
+import mad4124.team_sundry.task.databinding.FragmentCategoryListBinding;
 
 @AndroidEntryPoint
 public class CategoryListFragment extends Fragment {
+
+    FragmentCategoryListBinding binding = null;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if(binding == null){
+            binding = FragmentCategoryListBinding.inflate(inflater,container, false);
+        }
+        return binding.getRoot();
+    }
 }

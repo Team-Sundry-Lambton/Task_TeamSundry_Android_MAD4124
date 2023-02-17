84e5b4f0e7b4494ccac7b738bcbe3837f2fddbad
BaoTrieuThai206 <c0866206@mylambton.ca>
2023-02-17 17:29:14 -0500

    Fix db version

--- app/src/main/java/mad4124/team_sundry/task/db/AppDatabase.java
+++ app/src/main/java/mad4124/team_sundry/task/db/AppDatabase.java
@@ -19,7 +19,7 @@ import mad4124.team_sundry.task.model.Task;
         SubTask.class,
         MapLocation.class,
         MediaFile.class
-}, version = 2, exportSchema = false)
+}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
     private static final String DB_NAME = "task_room_db";
